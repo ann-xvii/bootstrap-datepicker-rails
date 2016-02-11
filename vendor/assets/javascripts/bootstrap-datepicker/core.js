9f1f3e9c8499fdd5ae677a74c6c658aa1c2eee1c
Anna Atiba <annamarie@iris.tv>
2016-02-10 18:37:51 -0800

    change min text, test

--- vendor/assets/javascripts/bootstrap-datepicker/core.js
+++ vendor/assets/javascripts/bootstrap-datepicker/core.js
@@ -1795,7 +1795,7 @@
 		en: {
 			days: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
 			daysShort: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
-			daysMin: ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"],
+			daysMin: ["S", "M", "T", "W", "T", "F", "S"],
 			months: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
 			monthsShort: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
 			today: "Today",
